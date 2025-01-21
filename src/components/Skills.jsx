d9cba61e86d3052edc22e0f119ee1ef617f020ea
MijbahMERN <misbhakhandoker@gmail.com>
2025-01-21 15:13:16 +0600

    skills page update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,3 @@
-
 import { FaHtml5, FaNodeJs } from "react-icons/fa";
 import { FaCss3Alt, FaGitAlt, FaReact } from "react-icons/fa6";
 import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
@@ -7,23 +6,59 @@ import { MeteorsDemo } from "./ui/MeteorsDemo";
 
 const Skills = () => {
   return (
-    <> <h1 className="py-12 text-5xl font-bold text-center text-yellow-500">Skills</h1>
-       <div id="skills" className="flex flex-wrap items-center justify-center w-full h-screen px-5 gap-x-7 ">
-      <MeteorsDemo title="HTML" color={"htmlColor"} image={<FaHtml5 />} />
-      <MeteorsDemo title="CSS" color={"cssColor"} image={<FaCss3Alt />} />
-      <MeteorsDemo title="JavaScript" color={"javaScriptColor"} image={<FaCss3Alt />} />
-      <MeteorsDemo title="TypeScript" color={"typeColor"} image={<SiTypescript />} />
-      <MeteorsDemo title="Tailwind Css" color={"tailwindColor"} image={<RiTailwindCssFill />} />
-      <MeteorsDemo title="React js" color={"reactColor"} image={<FaReact />}/>
-      <MeteorsDemo title="Node js" color={"nodeColor"} image={<FaNodeJs />} />
-      <MeteorsDemo title="Express js" color={"nextColor"} image={<SiExpress />} />
-      <MeteorsDemo title="Mongodb js" color={"mongodbColor"} image={<SiMongodb />} />
-      <MeteorsDemo title="Next js" color={"nextColor"} image={<RiNextjsFill />} />
-      <MeteorsDemo title="Git" color={"gitColor"} image={<FaGitAlt />} />
-      {/* <StickyScrollRevealDemo /> */}
-    </div>
+    <>
+      <div id="skills">
+        <h1 className="py-12 text-5xl font-bold text-center text-yellow-500">
+          Skills
+        </h1>
+        <div className="flex flex-wrap items-center justify-center w-full h-screen px-5 gap-x-7 ">
+          <MeteorsDemo title="HTML" color={"htmlColor"} image={<FaHtml5 />} />
+          <MeteorsDemo title="CSS" color={"cssColor"} image={<FaCss3Alt />} />
+          <MeteorsDemo
+            title="JavaScript"
+            color={"javaScriptColor"}
+            image={<FaCss3Alt />}
+          />
+          <MeteorsDemo
+            title="TypeScript"
+            color={"typeColor"}
+            image={<SiTypescript />}
+          />
+          <MeteorsDemo
+            title="Tailwind Css"
+            color={"tailwindColor"}
+            image={<RiTailwindCssFill />}
+          />
+          <MeteorsDemo
+            title="React js"
+            color={"reactColor"}
+            image={<FaReact />}
+          />
+          <MeteorsDemo
+            title="Node js"
+            color={"nodeColor"}
+            image={<FaNodeJs />}
+          />
+          <MeteorsDemo
+            title="Express js"
+            color={"nextColor"}
+            image={<SiExpress />}
+          />
+          <MeteorsDemo
+            title="Mongodb js"
+            color={"mongodbColor"}
+            image={<SiMongodb />}
+          />
+          <MeteorsDemo
+            title="Next js"
+            color={"nextColor"}
+            image={<RiNextjsFill />}
+          />
+          <MeteorsDemo title="Git" color={"gitColor"} image={<FaGitAlt />} />
+          {/* <StickyScrollRevealDemo /> */}
+        </div>
+      </div>
     </>
-   
   );
 };
 
